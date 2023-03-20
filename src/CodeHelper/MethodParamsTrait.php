b9c6df7ba46630ba92bbd86bc76d1d5f98569316
Andreas Sundqvist <andreas@stefna.is>
2023-03-20 16:24:26 +0100

    Fix rendering of method calls with multiple arrays in them

--- src/CodeHelper/MethodParamsTrait.php
+++ src/CodeHelper/MethodParamsTrait.php
@@ -17,85 +17,86 @@ trait MethodParamsTrait
 	{
 		$return = [];
 		$firstLine = $this->identifier . $this->callIdentifier . $this->method . '(';
-		if (count($this->params)) {
-			$paramSource = $this->getSourceForParams();
-			if (count($paramSource) === 1) {
-				if (is_string($paramSource[0]) && strlen($paramSource[0]) < 100) {
-					return [$firstLine . $paramSource[0] . ')'];
-				}
-				elseif (is_array($paramSource[0])) {
-					$firstParamSource = array_shift($paramSource[0]);
-					if (!is_string($firstParamSource)) {
-						throw InvalidCode::invalidType();
-					}
-					$return = [$firstLine . $firstParamSource];
-					foreach ($paramSource[0] as $row) {
-						$return[] = $row;
-					}
-					$lastRowKey = (int)array_key_last($return);
-					if (!is_string($return[$lastRowKey])) {
-						throw InvalidCode::invalidType();
-					}
-					$return[$lastRowKey] .= ')';
-					return $return;
-				}
+		if (!count($this->params)) {
+			$return[] = $firstLine . ')';
+			return $return;
+		}
+
+		$paramSource = $this->getSourceForParams();
+		if (count($paramSource) === 1) {
+			if (is_string($paramSource[0]) && strlen($paramSource[0]) < 100) {
+				return [$firstLine . $paramSource[0] . ')'];
 			}
-			if (is_string($paramSource[0]) && strpos($paramSource[0], ',')) {
-				$firstParamSource = array_shift($paramSource);
+			if (is_array($paramSource[0])) {
+				$firstParamSource = array_shift($paramSource[0]);
 				if (!is_string($firstParamSource)) {
 					throw InvalidCode::invalidType();
 				}
-				$return[] = $firstLine . $firstParamSource;
-			}
-			else {
-				$return[] = $firstLine;
-				$params = count($paramSource);
-				$renderedValues = [];
-				$lastValueIsArray = is_array($paramSource[$params - 1]);
-				foreach ($paramSource as $key => $row) {
-					if (is_string($row)) {
-						$value = $row;
-						if ($key !== $params - 1) {
-							$value .= ',';
-						}
-						$renderedValues[] = $value;
-					}
-					else {
-						$renderedValues[] = $row;
-					}
+				$return = [$firstLine . $firstParamSource];
+				foreach ($paramSource[0] as $row) {
+					$return[] = $row;
 				}
-				if ($lastValueIsArray) {
-					$renderedValues[count($renderedValues) - 1] = ')';
+				$lastRowKey = (int)array_key_last($return);
+				if (!is_string($return[$lastRowKey])) {
+					throw InvalidCode::invalidType();
 				}
-				$return[] = $renderedValues;
-				if (!$lastValueIsArray) {
-					$return[] = ')';
-				}
+				$return[$lastRowKey] .= ')';
 				return $return;
 			}
-			foreach ($paramSource as $row) {
-				$return[] = $row;
-			}
+		}
 
-			$lastRowKey = (int)array_key_last($return);
-			$lastRow = $return[$lastRowKey];
-			// if last param is an array append closing parentis
-			if (is_string($return[$lastRowKey]) && $return[$lastRowKey] === ']') {
-				$return[$lastRowKey] .= ')';
+		if (is_string($paramSource[0]) && strpos($paramSource[0], ',')) {
+			$firstParamSource = array_shift($paramSource);
+			if (!is_string($firstParamSource)) {
+				throw InvalidCode::invalidType();
 			}
-			// if last param have multiple values append closing parentis
-			elseif (is_string($return[$lastRowKey]) && strpos($return[$lastRowKey], ',')) {
-				$return[$lastRowKey] .= ')';
-			}
-			else {
-				$return[] = ')';
-			}
-
-			return $return;
+			$return[] = $firstLine . $firstParamSource;
 		}
 		else {
-			$return[] = $firstLine . ')';
+			$return[] = $firstLine;
+			$params = count($paramSource);
+			$renderedValues = [];
+			$lastValueIsArray = is_array($paramSource[$params - 1]);
+			foreach ($paramSource as $key => $row) {
+				if (is_string($row)) {
+					$value = $row;
+					if ($key !== $params - 1 && $value !== '[') {
+						$value .= ',';
+					}
+					$renderedValues[] = $value;
+				}
+				else {
+					$renderedValues[] = $row;
+				}
+			}
+			if ($lastValueIsArray) {
+				$renderedValues[count($renderedValues) - 1] = ')';
+			}
+			$return[] = $renderedValues;
+			if (!$lastValueIsArray) {
+				$return[] = ')';
+			}
+			return $return;
 		}
+
+		foreach ($paramSource as $row) {
+			$return[] = $row;
+		}
+
+		$lastRowKey = (int)array_key_last($return);
+		$lastRow = $return[$lastRowKey];
+		// if last param is an array append closing parentis
+		if (is_string($return[$lastRowKey]) && $return[$lastRowKey] === ']') {
+			$return[$lastRowKey] .= ')';
+		}
+		// if last param have multiple values append closing parentis
+		elseif (is_string($return[$lastRowKey]) && strpos($return[$lastRowKey], ',')) {
+			$return[$lastRowKey] .= ')';
+		}
+		else {
+			$return[] = ')';
+		}
+
 		return $return;
 	}
 
@@ -134,53 +135,61 @@ trait MethodParamsTrait
 		$return = [];
 		$previousArray = false;
 		$currentIndex = 0;
+		$paramCount = count($this->params);
+		$noCustomFirstLine = $paramCount > 4;
 		foreach ($this->params as $key => $param) {
-			if ($param instanceof CodeInterface) {
-				if ($param instanceof ArrayCode) {
-					if (isset($return[$currentIndex - 1]) && is_string($return[$key - 1])) {
-						$value = $param->getSourceArray();
-						$return[$currentIndex - 1] .= ', ' . array_shift($value);
-						foreach ($value as $x) {
-							$return[] = $x;
-							$currentIndex += 1;
-						}
-					}
-					else {
-						$return[] = $param->getSourceArray();
+			$isLast = $key - 1 === $paramCount;
+			if (!$param instanceof CodeInterface) {
+				$return[] = FormatValue::format($param);
+				$currentIndex += 1;
+				continue;
+			}
+
+			if ($param instanceof ArrayCode) {
+				$previousIndex = $currentIndex - 1;
+				if (!$noCustomFirstLine && $previousIndex === 0 && isset($return[$previousIndex]) && is_string($return[$key - 1])) {
+					$value = $param->getSourceArray();
+					$return[$currentIndex - 1] .= ', ' . array_shift($value);
+					foreach ($value as $x) {
+						$return[] = $x;
 						$currentIndex += 1;
 					}
-					$previousArray = true;
 				}
 				else {
-					if ($param instanceof VariableReference) {
-						$value = $param->toString();
+					foreach ($param->getSourceArray() as $v) {
+						$return[] = $v;
 					}
-					$value = $param->getSourceArray();
-					if ($previousArray) {
-						$tmpValue = array_shift($value);
-						if (!is_scalar($tmpValue)) {
-							throw InvalidCode::invalidType();
-						}
-						$return[$currentIndex - 1] .= ', ' . $tmpValue;
-						foreach ($value as $c) {
-							$return[] = $c;
-							$currentIndex += 1;
-						}
-					}
-					elseif (count($value) === 1) {
-						$return[] = $value[0];
-					}
-					else {
-						$return[] = $value;
-					}
+					$currentIndex += 1;
+				}
+				$previousArray = true;
+				continue;
+			}
 
+			if ($param instanceof VariableReference) {
+				$value = $param->toString();
+			}
+			$value = $param->getSourceArray();
+			if ($previousArray) {
+				$tmpValue = array_shift($value);
+				if (!is_scalar($tmpValue)) {
+					throw InvalidCode::invalidType();
+				}
+				if ($return[$currentIndex - 1] !== '[') {
+					$return[$currentIndex - 1] .= ', ' . $tmpValue;
+				}
+				foreach ($value as $c) {
+					$return[] = $c;
 					$currentIndex += 1;
 				}
 			}
-			else {
-				$return[] = FormatValue::format($param);
-				$currentIndex += 1;
+			elseif (count($value) === 1) {
+				$return[] = $value[0];
 			}
+			else {
+				$return[] = $value;
+			}
+
+			$currentIndex += 1;
 		}
 		return $return;
 	}
--- tests/CodeHelper/InstantiateClassTest.php
+++ tests/CodeHelper/InstantiateClassTest.php
@@ -8,10 +8,14 @@ use Stefna\PhpCodeBuilder\CodeHelper\InstantiateClass;
 use Stefna\PhpCodeBuilder\CodeHelper\StaticMethodCall;
 use Stefna\PhpCodeBuilder\CodeHelper\VariableReference;
 use Stefna\PhpCodeBuilder\FlattenSource;
+use Stefna\PhpCodeBuilder\Renderer\Php7Renderer;
+use Stefna\PhpCodeBuilder\Tests\Renderer\AssertResultTrait;
 use Stefna\PhpCodeBuilder\ValueObject\Identifier;
 
 final class InstantiateClassTest extends TestCase
 {
+	use AssertResultTrait;
+
 	public function testSimple(): void
 	{
 		$call = new InstantiateClass(Identifier::fromString(StaticMethodCall::class), []);
@@ -39,5 +43,60 @@ final class InstantiateClassTest extends TestCase
 	\'scheme\' => \'bearer\',
 	\'description\' => \'Valid for site specific endpoints\',
 ], $testVar)', trim(FlattenSource::source($call->getSourceArray())));
+	}
+
+	public function testMixedParamsInInstantiateClass(): void
+	{
+		$x = new InstantiateClass(
+			Identifier::fromString('self'),
+			[
+				'random',
+				'random',
+				new ArrayCode(['id']),
+				new ArrayCode(['a', 'b', 'c', 'd', 'e', 'f']),
+				null,
+				null,
+			],
+		);
+
+		$source = $x->getSourceArray();
+		$this->assertSame([
+			'new self(',
+			[
+				"'random',",
+				"'random',",
+				'[',
+				[
+					"'id',",
+				],
+				'],',
+				'[',
+				["'a',", "'b',", "'c',", "'d',", "'e',", "'f',"],
+				'],',
+				'null,',
+				'null',
+			],
+			')',
+		], $source);
+
+		$this->assertSourceResult(FlattenSource::source($source), 'InstantiateClassTest.testMixedParamsInInstantiateClass');
+	}
+
+	public function testMixedParamsWithArrayAsSecondArgument(): void
+	{
+		$x = new InstantiateClass(
+			Identifier::fromString('self'),
+			[
+				'random',
+				new ArrayCode(['id']),
+				new ArrayCode(['a', 'b', 'c', 'd', 'e', 'f']),
+				null,
+				null,
+			],
+		);
+
+		$source = $x->getSourceArray();
+
+		$this->assertSourceResult(FlattenSource::source($source), 'InstantiateClassTest.testMixedParamsWithArrayAsSecondArgument');
 	}
 }
